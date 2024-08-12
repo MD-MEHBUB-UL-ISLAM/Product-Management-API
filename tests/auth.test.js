94932641982fd45c6dbddf9bf1e1bffde8e1b54c
MD MEHBUB UL ISLAM <114100350+MD-MEHBUB-UL-ISLAM@users.noreply.github.com>
2024-08-12 10:26:42 +0600

    Fourth

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../app');
 const sequelize = require('../config/database');
 
-beforeAll(async () => {
+before(async () => {
   await sequelize.sync({ force: true });
 });
 
